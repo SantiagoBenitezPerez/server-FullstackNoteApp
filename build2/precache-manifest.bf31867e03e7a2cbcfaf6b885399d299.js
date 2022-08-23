9a61d37d9147b9aa85dd27424281568aef6870f3
Santiago Benitez <A01782813@tec.mx>
2022-08-22 22:23:39 -0500

    add build2

---
+++ build2/precache-manifest.bf31867e03e7a2cbcfaf6b885399d299.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f77c058ab014ddebd967",
+    "url": "/static/css/main.1ef7e9b2.chunk.css"
+  },
+  {
+    "revision": "f77c058ab014ddebd967",
+    "url": "/static/js/main.f77c058a.chunk.js"
+  },
+  {
+    "revision": "4422e644433f4a61be16",
+    "url": "/static/js/1.4422e644.chunk.js"
+  },
+  {
+    "revision": "14bbddc9299f1ed9a1a8",
+    "url": "/static/css/2.08d200cd.chunk.css"
+  },
+  {
+    "revision": "14bbddc9299f1ed9a1a8",
+    "url": "/static/js/2.14bbddc9.chunk.js"
+  },
+  {
+    "revision": "9c15d78cc6e756e21791",
+    "url": "/static/js/runtime~main.9c15d78c.js"
+  },
+  {
+    "revision": "a1c79315275febab986799bd897e37ff",
+    "url": "/index.html"
+  }
+];
